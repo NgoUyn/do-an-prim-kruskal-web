f54efa7a815530cd953ce44aa4dce8c95049229e
longpham <longkul1478@gmail.com>
2025-10-24 16:26:17 +0700

    Reinitialize local repo after unzip

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prim_Kruskal_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index() // giới thiệu về giải thuật tham lam, prim and kruskal
+        {
+            return View();
+        }
+
+        public ActionResult MoPhong() // mô phỏng trực quan
+        {
+            return View();
+        }
+         
+        public ActionResult UngDung()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+        }
+}
